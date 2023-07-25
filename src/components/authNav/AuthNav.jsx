56b0be548caec1837faaa3fa102f2ef970a3a77c
VladSurzhko <surzhkovlad69@gmail.com>
2023-07-25 22:30:25 +0200

    update Auth

--- src/components/authNav/AuthNav.jsx
+++ src/components/authNav/AuthNav.jsx
@@ -5,7 +5,7 @@ export const AuthNav = () => {
   return (
     <WrapperAuthNav>
       <li>
-        <StyledLink to="/register">Register</StyledLink>
+        <StyledLink to="/register">Sign Up</StyledLink>
       </li>
       <li>
         <StyledLink to="/login">Log In</StyledLink>
